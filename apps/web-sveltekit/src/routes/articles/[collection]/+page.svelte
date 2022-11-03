238b569b91022e54077ead31572dc073b3805bd0
Jørn Kinderås <jorn.kinderas@makingwaves.com>
2022-11-03 20:09:27 +0100

    Adds some basic SEO to the SveltKit site

--- apps/web-sveltekit/src/routes/articles/[collection]/+page.svelte
+++ apps/web-sveltekit/src/routes/articles/[collection]/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+	import { page } from "$app/stores";
+	import SEO from "$lib/components/SEO.svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+
+	const collection = data.articles[0].collection;
 </script>
 
+<SEO
+	pageType={"website"}
+	title={`All articles in ${collection.name}`}
+	description={collection.description || ""}
+	permalink={$page.url.pathname}
+	publishDate={collection._createdAt}
+/>
 <h2>Articles in collection «{data.articles[0].collection.name}»</h2>
 <ul>
 	{#each data.articles as article}
--- apps/web-sveltekit/src/routes/articles/[collection]/[slug]/+page.svelte
+++ apps/web-sveltekit/src/routes/articles/[collection]/[slug]/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import { page } from "$app/stores";
+	import SEO from "$lib/components/SEO.svelte";
 	import ArticlePageComponent from "$lib/components/pageComponents/articlePageComponent.svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
 </script>
 
+<SEO
+	pageType={"article"}
+	title={data.article.title}
+	description={data.article.description}
+	permalink={$page.url.pathname}
+	publishDate={data.article._createdAt}
+/>
 <ArticlePageComponent article={data.article} />
